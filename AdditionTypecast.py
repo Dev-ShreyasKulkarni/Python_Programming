f41bc3c32910c770fac8e712501d3bab96121d1c
Shreyas Kulkarni <shreyaskulkarni2212@gmail.com>
2026-06-13 21:04:05 +0530

    13 June 2026

---
+++ AdditionTypecast.py
@@ -0,0 +1,10 @@
+print("Enter first number : ")
+no1 = int(input())
+print("Enter second number : ")
+no2 = int(input())
+
+print(type(no1))
+print(type(no2))
+
+Ans = no1 + no2
+print("Addition is : ",Ans)
--- Age.py
+++ Age.py
@@ -2,5 +2,7 @@ age = int(input("Enter your age: "))
 
 if age > 18 :
     print("You are an adult")
+elif age <= 0:
+    print("Age cannot be negative or zero")
 else :
     print("You are underage")
